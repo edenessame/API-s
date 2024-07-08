c85727f6d76109ea6a3b56aab01f8d5f40ae9d0a
Eden Essame <eden_essame80@hotmail.com>
2024-07-08 15:40:14 +0000

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,10 +76,6 @@ async function postForm(e) {
     }
      */
 
-    for (let entry of form.entries()) {
-        console.log(entry);
-    }
-
     const response = await fetch(API_URL, {
         method: "POST",
         headers: {
